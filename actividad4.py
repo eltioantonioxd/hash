f69ec0e77a7f385af40f81ace90b7e551a52a749
eltioantonioxd <91589175+eltioantonioxd@users.noreply.github.com>
2021-10-21 18:31:13 -0300

    Update actividad4.py

--- actividad4.py
+++ actividad4.py
@@ -1,6 +1,8 @@
 import string
 import math
 import hashlib
+import re
+import time
 
 base = str(string.ascii_letters + string.digits + string.punctuation)
 base2 = str(string.punctuation)
@@ -26,6 +28,17 @@ def resumen(password):
     Funcion5 = str(int(math.sqrt(F3(password))))
     return (Funcion2 + Funcion3 + Funcion4 + Funcion1 + Funcion5)
 
+def min_mas(password):
+    contador = {'minusculas': 0, 'mayusculas': 0, 'tílde':0}
+    for i in password:
+        if re.search(r'[À-ÿ]', i):
+            contador['tílde'] += 1
+        elif i.islower():
+            contador['minusculas'] += 1
+        elif i.isupper():
+            contador['mayusculas'] += 1
+    return contador
+
 def F2(password):
     match = ""
     cont=0
@@ -40,7 +53,6 @@ def F2(password):
         match = base[int(base.find(password[0])/2) + 7] + base2[int(base.find(password[len(password)-1])/2)] + base[int(base.find(password[len(password)-2])/2)] +"<#?"
         return match
     
-
 def F3(password):
     pares = 0
     impares = 0
@@ -54,36 +66,66 @@ def F3(password):
 def F4(password):
     ofuscar = ""
     cont = 0
-    if(len(password)%2 == 0 and password.isalnum() == True):
-        for i in range(0, len(base2), 3):
-            ofuscar += base2[i] + base[i]
+    minMax = min_mas(password)
+    if(minMax['tílde']):
+        for i in range(0, len(base2), 2):
+            ofuscar += base2[i] + base[i] 
             cont +=1
             if(cont >= 4):
-                return ofuscar
-    elif(len(password)%2 == 0 and password.isalnum() == False):
+                return ofuscar + password[0] 
+
+    elif(minMax['mayusculas'] > 0 and minMax['minusculas'] > 0):
+        for i in range(0, len(base2), 3):
+            ofuscar += base2[i] + base[i] 
+            cont +=1
+            if(cont >= 4):
+                return ofuscar + password[len(password)-1]
+
+    elif(minMax['minusculas'] > 0 and minMax['mayusculas'] == False and len(password)%2 == 0):
         for i in range(0, len(base2), 4):
-            ofuscar += base2[i] + base[i]
+            ofuscar += base2[i] + base[i] 
             cont +=1 
             if(cont >= 4):
-                return ofuscar
-    else:
+                return ofuscar + password[len(password)-1]
+    elif(minMax['minusculas'] > 0 and minMax['mayusculas'] == False and len(password)%2 != 0):
+        for i in range(0, len(base2), 7):
+            ofuscar += base2[i] + base[i] 
+            cont +=1 
+            if(cont >= 4):
+                return ofuscar + password[0] 
+
+    elif(minMax['minusculas'] == False and minMax['mayusculas'] > 0 and len(password)%2 == 0):
         for i in range(0, len(base2), 8):
             ofuscar += base2[i] + base[i]
             cont +=1
             if(cont >= 4):
-                return ofuscar
+                return ofuscar + password[len(password)-1]
+
+    elif(minMax['minusculas'] == False and minMax['mayusculas'] > 0 and len(password)%2 != 0):
+        for i in range(0, len(base2), 6):
+            ofuscar += base2[i] + base[i] 
+            cont +=1
+            print('hola')
+            if(cont >= 4):
+                return ofuscar + password[0] 
+    elif(password.isdigit()):
+        return F3(password)
 
 def lectura(fichero):
-    with open("./"+fichero, "r", encoding='utf8') as file:
-        cont = 0
-        for line in file:
-            palabra = line.strip()
-            print("Contraseña:", palabra, "Hash por defecto:",  hashR(resumen(palabra), palabra))
-            cont += 1
-            if(cont == 50):
-                return file.close()
-def entropia(password):
-    H = len(password) * math.log2(len(base))
+    try:
+        with open(fichero, "r", encoding='utf8') as file:
+            cont = 0
+            for line in file:
+                palabra = line.strip()
+                print("Contraseña:", palabra, "-------> Hash por defecto:",  hashR(resumen(palabra), palabra))
+                cont += 1
+                if(cont == 50):
+                    return file.close()
+    except:
+        return print('Algo salio mal :(. Corrobora que la ubicación, nombre y extensión del fichero esten correctos')
+
+def entropia(password, Base):
+    H = len(password) * math.log2(Base)
     return H
 
 def switch():
@@ -93,34 +135,64 @@ def switch():
     print('3.- SHA1')
     print('4.- SHA256')
     print('5.- MDN5')
-
-    opcion = int(input())
     
-    if(opcion == 1):
-        fichero = input("ingrese el nombre y la extensión del fichero: ")
-        lectura(fichero)
+    try:
+        seguir = True
+        while(seguir):
+            opcion = int(input())
+            
+            if(opcion == 1):
+                fichero = input("ingrese la ubicación, nombre y extensión del fichero: ")
+                lectura(fichero)
+                seguir = False
 
-    elif(opcion ==2):
-        password = input("ingrese contraseña: ")
-        contraseña = resumen(password)
-        hash25 = hashR(contraseña, password)
-        print("Hash creado:", hash25, "     Entropía:", entropia(hash25))
+            elif(opcion ==2):
+                password = input("ingrese contraseña: ")
+                inicio = time.time()
+                contraseña = resumen(password)
+                hash25 = hashR(contraseña, password)
+                print("Hash creado:", hash25, "     Entropía:", entropia(hash25, 94))
+                fin = time.time()
+                delta = fin-inicio
+                print("Velocidad hash", delta)
+                seguir = False
 
-    elif(opcion == 3):
-        password = input("ingrese contraseña: ")
-        string_to_hash = password
-        hash_object = hashlib.sha1(str(string_to_hash).encode('utf-8'))
-        print('Sha1: ', hash_object.hexdigest(), "     Entropía:", entropia(hash_object.hexdigest()))
+            elif(opcion == 3):
+                password = input("ingrese contraseña: ")
+                string_to_hash = password
+                inicio = time.time()
+                hash_object = hashlib.sha1(str(string_to_hash).encode('utf-8'))
+                print('Sha1: ', hash_object.hexdigest(), "     Entropía:", entropia(hash_object.hexdigest(), 16))
+                fin = time.time()
+                delta = fin-inicio
+                print("Velocidad hash", delta)
+                seguir = False
 
-    elif(opcion == 4):
-        password = input("ingrese contraseña: ")
-        string_to_hash = password
-        hash_object = hashlib.sha256(str(string_to_hash).encode('utf-8'))
-        print('Sha256: ', hash_object.hexdigest(), "     Entropía:", entropia(hash_object.hexdigest()))
-    
-    elif(opcion == 5):
-        password = input("ingrese contraseña: ")
-        string_to_hash = password
-        hash_object = hashlib.md5(str(string_to_hash).encode('utf-8'))
-        print('MD5: ', hash_object.hexdigest(), "     Entropía:", entropia(hash_object.hexdigest()))
-switch()
+            elif(opcion == 4):
+                password = input("ingrese contraseña: ")
+                string_to_hash = password
+                inicio = time.time()
+                hash_object = hashlib.sha256(str(string_to_hash).encode('utf-8'))
+                print('Sha256: ', hash_object.hexdigest(), "     Entropía:", entropia(hash_object.hexdigest(), 16))
+                fin = time.time()
+                delta = fin-inicio
+                print("Velocidad hash", delta)
+                seguir = False
+
+            elif(opcion == 5):
+                password = input("ingrese contraseña: ")
+                string_to_hash = password
+                inicio = time.time()
+                hash_object = hashlib.md5(str(string_to_hash).encode('utf-8'))
+                print('MD5: ', hash_object.hexdigest(), "     Entropía:", entropia(hash_object.hexdigest(), 16))
+                fin = time.time()
+                delta = fin-inicio
+                print("Velocidad hash", delta)
+                seguir = False
+            else:
+                print('Favor escoga una opción válida')
+    except ValueError:
+        return False
+
+if(switch() == False):
+    print('Ups, al parecer no ingreso un dígito')
